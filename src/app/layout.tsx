dbfc9b2c2cea9a41f048c293b94117a5bd5f6b29
Ankit Mittal <mittalankit.21.2008@gmail.com>
2025-10-18 15:14:26 +0530

    03:trpcSetup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TRPCReactProvider } from "@/trpc/client";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <TRPCReactProvider> 
+          {children}
+        </TRPCReactProvider>
+       
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
-import prisma from "@/lib/prisma"
+import { caller, getQueryClient, trpc } from "@/trpc/server"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
+import { Suspense } from "react"
+import { Client } from "./client"
 
 
 
-
-const Page = async() => {
-
-    const user = await prisma.user.findMany()
+const Page =  async () => {
+    //const users =  await caller.getUsers()
+    const queryClient = getQueryClient()
+    const users =  queryClient.prefetchQuery(trpc.getUsers.queryOptions())
 
     return <div>
 
-      {JSON.stringify(user)}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Client/>
+        </Suspense>
+        
+        </HydrationBoundary>
+      
     </div>
 }
 
